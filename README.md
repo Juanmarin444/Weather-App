83f93e3577f1dfbc4b6a83e70788c6497c6a8dd7
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-08-13 16:20:41 -0700

    Add Full Project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather App
+
+You can download this weather app to your desktop!
+
+## Weather API
+
+The API use in this project:
+https://openweathermap.org/api
+
+### The Search Bar
+
+The search bar allows you to look a the weather in various areas.
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const URL = process.env.REACT_APP_API_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    }
+  });
+  return data;
+}
